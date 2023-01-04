4b35793f9b7dc527130d8672e6a6799228b5e8cc
Jack Doy <jackdoy99@icloud.com>
2023-01-04 17:32:34 +0000

    Added about me page and updated animations on homepage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,25 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import AnimatedTextWord from "./Animations/AnimatedTextWord";
-import AnimatedTextWord2 from "./Animations/AnimatedTextWord2";
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
 
 const Hero = () => {
   return (
     <>
       <div className="flex items-center text-center justify-center h-screen custom-img-hero">
         <div className="text-white z-[2] w-full">
-          <h1 className="inline-flex justify-center items-center text-center">
-            <AnimatedTextWord text="CB" />
-          </h1>
-          <h1 className="inline-flex justify-center items-center text-center">
-            <AnimatedTextWord2 text="RECRUITMENT" />
-          </h1>
-          <p className="italic text-white mt-2 max-w-[80%] mx-auto min-[500px]:max-w-[60%]">
-            Number one business partner, for all your{" "}
-            <span className="text-blue-600">Crypto</span> and{" "}
-            <span className="text-blue-600">Blockchain</span> needs.
-          </p>
-          <button className="px-6 py-2 mt-7 bg-gradient-to-b from-[#382AC3] to to-[#02AFE5] rounded-full text-white">
+          <div className="animate-in slide-in-from-top duration-700">
+            <h1 className=" inline-flex justify-center items-center text-center font-medium mobile-font min-[400px]:text-5xl sm:text-6xl md:text-7xl xl:text-8xl text-white">
+              CB
+              <span className="font-light text-transparent bg-clip-text bg-gradient-to-tr from-[#382AC3] to-[#02AFE5] mobile-font min-[400px]:text-5xl sm:text-6xl md:text-7xl xl:text-8xl">
+                Recruitment
+              </span>
+            </h1>
+            <p className="italic text-white mt-2 max-w-[80%] mx-auto min-[500px]:max-w-[60%]">
+              Number one business partner, for all your <span className="text-blue-600">Crypto</span> and{" "}
+              <span className="text-blue-600">Blockchain</span> needs.
+            </p>
+          </div>
+          <button className="animate-in fade-in-10 duration-1000 px-6 py-2 mt-7 bg-gradient-to-b from-[#382AC3] to to-[#02AFE5] rounded-full text-white">
             Get Started &#8599;
           </button>
           <div className="absolute bottom-0 w-full flex justify-center space-x-8 mb-10">
@@ -42,10 +41,7 @@ const Hero = () => {
                 height={35}
               />
             </Link>
-            <Link
-              href="https://www.linkedin.com/company/80659134"
-              target="_blank"
-            >
+            <Link href="https://www.linkedin.com/company/80659134" target="_blank">
               <Image
                 className="hover:scale-110 ease-in-out duration-200"
                 src={"/assets/linkedin-white.png"}
@@ -67,7 +63,7 @@ const Hero = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
