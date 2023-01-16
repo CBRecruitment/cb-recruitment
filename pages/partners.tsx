14bfba3e03d9616f0282e6c79cd9b4d36a34e4d4
Jack Doy <jackdoy99@icloud.com>
2023-01-16 09:32:27 +0000

    test

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const partners = () => {
   return (
-    <div className= 'flex items-center text-center justify-center h-screen custom-img-hero'>
-        <h1 className='flex justify-center text-white text-6xl'>Partners</h1>
+    <div className='flex items-center text-center justify-center h-screen custom-img-hero'>
+      <h1 className='flex justify-center text-white text-6xl'>Partners!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default partners
+export default partners;
