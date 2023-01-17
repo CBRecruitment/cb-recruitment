1c2e1648d02b2deb6a398cad0f10e6d2fa554761
Jack Doy <jackdoy99@icloud.com>
2023-01-17 12:41:24 +0000

    test commit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const contact = () => {
   return (
     <div className= 'flex items-center text-center justify-center h-screen custom-img-hero'>
-        <h1 className='flex justify-center text-white text-6xl'>Contact</h1>
+        <h1 className='flex justify-center text-white text-6xl'>Contact!</h1>
     </div>
   )
 }
