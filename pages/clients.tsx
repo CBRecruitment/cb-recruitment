4c37fc73d5958a609160027687c3f9a58bc6ea61
Jack Doy <jackdoy99@icloud.com>
2023-01-18 17:36:08 +0000

    Added job openings page and specific job pages per id

--- pages/clients.tsx
+++ pages/clients.tsx
@@ -1,38 +1,65 @@
+import Link from 'next/link';
 import React from 'react';
+const BullhornUrl = process.env.REACT_APP_BULLHORN_URL;
+const BhRestToken = process.env.REACT_APP_BH_REST_TOKEN;
 
-const publicBullhornUrl = process.env.REACT_APP_PUBLIC_BULLHORN_URL;
-
-const publicOpenJobEndpoint = `${publicBullhornUrl}/search/JobOrder?query=(isOpen:1)&count=1&sort=-dateLastPublished&start=0&fields=id,title,publicDescription`;
+const openJobEndpoint = `${BullhornUrl}/search/JobOrder?fields=id,title,categories,skills,employmentType,customText14,customText12&count=20&start=0&query=isOpen:1 AND isDeleted:0 AND NOT status:archive&BhRestToken=${BhRestToken}&sort=-dateLastPublished`;
 
 export async function getServerSideProps() {
-  const res = await fetch(publicOpenJobEndpoint);
+  const res = await fetch(openJobEndpoint);
   const data = await res.json();
   return { props: { data } };
 }
 
 const clients = ({ data }) => {
   const results = data.data;
-  console.log(results);
   return (
-    <div className='bg-gray-900 text-white h-screen'>
-      {/* <h1 className='text-4xl flex justify-center py-2'>Job Board</h1>
+    <div className='bg-gray-900 text-white h-full py-10'>
+      <h1 className='text-4xl flex justify-center underline text-[#1885E0]'>
+        Job Openings
+      </h1>
       <div className='w-[60%] mx-auto'>
-        <ul>
+        <div>
           {results.map((result) => {
+            const skills = result.skills.data;
             return (
-              <li key={result.id} className='flex space-x-4 text-xl'>
-                <h1>{result.title}</h1>
-                <p>{result.id}</p>
-                <p
-                  dangerouslySetInnerHTML={{ __html: result.publicDescription }}
-                ></p>
-              </li>
+              <Link href={`/job/${result.id}`}>
+                <div
+                  key={result.id}
+                  className='mt-6 flex justify-between items-center py-5 border border-white px-5 rounded-md hover:bg-[#1885E0] hover:text-black hover:border-black'
+                >
+                  <div>
+                    <p className='text-2xl'>{result.title}</p>
+                    <p className=''>{result.categories.data[0].name}</p>
+                  </div>
+                  <div className='flex flex-row space-x-5'>
+                    {skills.map((skill) => (
+                      <p
+                        key={result.id}
+                        className='text-sm rounded-md border border-blue-300 p-2'
+                      >
+                        {skill.name}
+                      </p>
+                    ))}
+                    <p className='text-sm rounded-md border border-blue-300 p-2'>
+                      {result.customText12}
+                    </p>
+                    <p className='text-sm rounded-md border border-blue-300 p-2'>
+                      {result.customText14}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             );
           })}
-        </ul>
-      </div> */}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default clients;
+
+{
+  /* <p dangerouslySetInnerHTML={{ __html: result.description }} /> */
+}
