ecad2da7aa3e0808de29fa4aeb339f18c01d6f83
JD <jackdoy99@icloud.com>
2023-02-09 15:25:09 +0000

    Blog updates

--- components/blog/meta.tsx
+++ components/blog/meta.tsx
@@ -37,7 +37,7 @@ const Meta = () => {
             <link rel='alternate' type='application/rss+xml' href='/feed.xml' />
             <meta
                 name='description'
-                content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+                content={`CBRecruitment Blog: Read informative crypto and blockchain articles such as "How to become a blockchain developer".`}
             />
             <meta property='og:image' content={HOME_OG_IMAGE_URL} />
         </Head>
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -21,7 +21,7 @@ export default function Blog({ allPosts }: Props) {
             <Navbar />
             <Layout>
                 <Head>
-                    <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+                    <title>{`Read Up-To-Date Content on Everything Crypto`}</title>
                 </Head>
                 <Container>
                     <Intro />
