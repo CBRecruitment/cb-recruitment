acccb9db0b28fee79cd2147d6d7bfe8e13cfb56d
JD <jackdoy99@icloud.com>
2023-02-14 13:55:02 +0000

    Update LearnMore.tsx

--- components/LearnMore.tsx
+++ components/LearnMore.tsx
@@ -12,18 +12,6 @@ const LearnMore = () => {
                     height={'0'}
                     className='absolute pt-10 left-[10px]'
                 />
-                <Image
-                    src='/assets/branding/Dotted_square.png'
-                    alt=''
-                    width={'120'}
-                    height={'0'}
-                    className='absolute top-[200px] left-[80px]'
-                />
-                <div className='w-fit absolute top-[300px] right-[20px] z-[2]'>
-                    <button className='px-6 py-1 rounded-full text-[14px] tracking-wide font-semibold bg-gradient-to-b from-[#f09c05] to-[#f5d11e] text-white'>
-                        LEARN MORE
-                    </button>
-                </div>
                 <div className='absolute top-[45px] left-[120px] w-fit Roboto-Bold'>
                     <h3 className='text-2xl'>NEW</h3>
                     <h2 className='text-3xl tracking-wide'>TO WEB3</h2>
@@ -32,7 +20,13 @@ const LearnMore = () => {
                     Short blurb about being able to support those with no web3
                     experience
                 </div>
-
+                <Image
+                    src='/assets/branding/Dotted_square.png'
+                    alt=''
+                    width={'120'}
+                    height={'0'}
+                    className='absolute top-[205px] left-[85px]'
+                />
                 <Image
                     src='/assets/branding/Dotted_line.png'
                     alt=''
@@ -40,6 +34,11 @@ const LearnMore = () => {
                     height={'0'}
                     className='absolute top-[7px] right-[23px]'
                 />
+                <div className='w-fit absolute top-[305px] right-[20px]'>
+                    <button className='px-6 py-1 rounded-full text-[14px] tracking-wide font-semibold bg-gradient-to-b from-[#f09c05] to-[#f5d11e] text-white'>
+                        LEARN MORE
+                    </button>
+                </div>
             </div>
         </div>
     );
