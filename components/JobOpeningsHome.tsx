c655b5b6ee278c02e704850d7d3f2b6ab31a5b60
JD <jackdoy99@icloud.com>
2023-02-14 15:07:40 +0000

    Removed api for testing

--- components/JobOpeningsHome.tsx
+++ components/JobOpeningsHome.tsx
@@ -1,26 +1,26 @@
-import Link from 'next/link';
-import { Job } from '../interfaces/types';
+// import Link from 'next/link';
+// import { Job } from '../interfaces/types';
 
-type Props = {
-    jobs: Job[];
-};
+// type Props = {
+//     jobs: Job[];
+// };
 
-const JobOpeningsHome = ({ jobs }: Props) => {
-    return (
-        <div className='bg-[var(--gray)] py-6 flex flex-col space-y-2 px-4'>
-            {jobs?.map((job) => {
-                return (
-                    <Link href={`/job/${job.id}`} key={job.id}>
-                        <div className='border-2 border-white rounded-lg w-fit p-1'>
-                            <p className='text-white Roboto-Bold'>
-                                {job.title}
-                            </p>
-                        </div>
-                    </Link>
-                );
-            })}
-        </div>
-    );
-};
+// const JobOpeningsHome = ({ jobs }: Props) => {
+//     return (
+//         <div className='bg-[var(--gray)] py-6 flex flex-col space-y-2 px-4'>
+//             {jobs?.map((job) => {
+//                 return (
+//                     <Link href={`/job/${job.id}`} key={job.id}>
+//                         <div className='border-2 border-white rounded-lg w-fit p-1'>
+//                             <p className='text-white Roboto-Bold'>
+//                                 {job.title}
+//                             </p>
+//                         </div>
+//                     </Link>
+//                 );
+//             })}
+//         </div>
+//     );
+// };
 
-export default JobOpeningsHome;
+// export default JobOpeningsHome;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,35 @@ import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import React from 'react';
 import Hero from '../components/Hero';
-import JobOpeningsHome from '../components/JobOpeningsHome';
+// import JobOpeningsHome from '../components/JobOpeningsHome';
 import LearnMore from '../components/LearnMore';
 import Navbar from '../components/Navbar';
 import Partners from '../components/Partners';
 import { Job } from '../interfaces/types';
 
-const BullhornUrl = process.env.REACT_APP_BULLHORN_URL;
-const BhRestToken = process.env.REACT_APP_BH_REST_TOKEN;
+// const BullhornUrl = process.env.REACT_APP_BULLHORN_URL;
+// const BhRestToken = process.env.REACT_APP_BH_REST_TOKEN;
 
-type SearchResponse = {
-    total: number;
-    start: number;
-    count: number;
-    data: Job[];
-};
+// type SearchResponse = {
+//     total: number;
+//     start: number;
+//     count: number;
+//     data: Job[];
+// };
 
-type Props = {
-    searchResults: SearchResponse;
-};
+// type Props = {
+//     searchResults: SearchResponse;
+// };
 
-export const getServerSideProps: GetServerSideProps = async (req) => {
-    const res = await fetch(
-        `${BullhornUrl}/search/JobOrder?fields=id,title&count=10&query=isOpen:1 AND isDeleted:0 AND NOT status:archive&BhRestToken=${BhRestToken}&sort=-dateAdded`
-    );
-    const searchResults: Props = await res.json();
-    return { props: { searchResults } };
-};
+// export const getServerSideProps: GetServerSideProps = async (req) => {
+//     const res = await fetch(
+//         `${BullhornUrl}/search/JobOrder?fields=id,title&count=10&query=isOpen:1 AND isDeleted:0 AND NOT status:archive&BhRestToken=${BhRestToken}&sort=-dateAdded`
+//     );
+//     const searchResults: Props = await res.json();
+//     return { props: { searchResults } };
+// };
 
-const Home = ({ searchResults }: Props) => {
+const Home = () => {
     return (
         <>
             <div className=''>
