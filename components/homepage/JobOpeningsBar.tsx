9743542098521d20b5b0eaa81b520dd3071f98e5
JD <jackdoy99@icloud.com>
2023-02-16 17:51:25 +0000

    Update JobOpeningsBar.tsx

--- components/homepage/JobOpeningsBar.tsx
+++ components/homepage/JobOpeningsBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const JobOpeningsBar = () => {
     return (
-        <div className='absolute top-[175px]'>
+        <div className='absolute top-[169px]'>
             <div className='absolute'>
                 <p className='bg-[var(--cream)] rounded-full m-auto w-fit px-[2rem] py-[0.2rem] text-[var(--darkgray)] text-[11px] Roboto font-semibold tracking-wide top-[14px] pt-1 relative bottom-0'>
                     JOB OPENINGS
