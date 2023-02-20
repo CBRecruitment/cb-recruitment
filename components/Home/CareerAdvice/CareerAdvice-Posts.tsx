2b244d668adb2a90eb6731d95bc157e58a3591bb
JD <jackdoy99@icloud.com>
2023-02-20 09:15:28 +0000

    Update CareerAdvice-Posts.tsx

--- components/Home/CareerAdvice/CareerAdvice-Posts.tsx
+++ components/Home/CareerAdvice/CareerAdvice-Posts.tsx
@@ -5,47 +5,34 @@ import CareerAdviceCover from './CareerAdvice-Cover';
 import styles from './CareerAdvice.module.css';
 
 type Props = {
-    title: string;
-    coverImage: string;
-    date: string;
-    excerpt: string;
-    author: Author;
-    slug: string;
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
 };
 
-const CareerAdvicePosts = ({
-    title,
-    coverImage,
-    date,
-    excerpt,
-    author,
-    slug,
-}: Props) => {
-    return (
-        <div className={styles.card}>
-            <div className='mb-3'>
-                <CareerAdviceCover slug={slug} title={title} src={coverImage} />
-            </div>
-            <div className='p-2'>
-                <h3 className='text-[10px] font-bold mb-3 leading-snug flex justify-center'>
-                    <Link
-                        as={`/${slug}`}
-                        href='/[slug]'
-                        className='hover:underline'
-                    >
-                        {title}
-                    </Link>
-                </h3>
-                <div className='text-[10px] flex justify-between w-[95%] m-auto'>
-                    <p>Jan Barley</p>
-                    <DateFormatter dateString={date} />
-                    <p className='text-[10px] leading-relaxed mb-4 bg-[var(--orange)] w-fit px-3 rounded-full'>
-                        {excerpt}
-                    </p>
-                </div>
-            </div>
+const CareerAdvicePosts = ({ title, coverImage, date, excerpt, author, slug }: Props) => {
+  return (
+    <div className={styles.card}>
+      <div className='mb-3'>
+        <CareerAdviceCover slug={slug} title={title} src={coverImage} />
+      </div>
+      <div className='p-2'>
+        <h3 className='text-[10px] font-bold mb-3 leading-snug flex justify-center'>
+          <Link as={`/${slug}`} href='/[slug]' className='hover:underline'>
+            {title}
+          </Link>
+        </h3>
+        <div className='text-[10px] flex justify-between w-[95%] m-auto'>
+          <p>Jan Barley</p>
+          <DateFormatter dateString={date} />
+          <p className='text-[10px] leading-relaxed mb-4 bg-[var(--orange)] w-fit px-3 rounded-full'>{excerpt}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CareerAdvicePosts;
